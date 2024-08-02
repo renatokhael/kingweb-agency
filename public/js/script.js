a032d11b51fec43f403c784e371f5d10cf939992
Renato Khael <renatokhael@gmail.com>
2024-08-02 03:56:15 -0300

    ✨ feat: contact form script

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+window.onload = function () {
+  document
+    .getElementById("contact-section")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm("contact_service", "contact_form", this).then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
